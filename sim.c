0cc144e558c97f1ec5a4ecc0c8cff8e57e21d5a8
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-10-18 14:22:59 +0800

    Create template for MPI

---
+++ sim.c
@@ -0,0 +1,37 @@
+#include <mpi.h>
+#include <stdio.h>
+
+#define MASTER_ID slaves
+
+int myid, slaves;
+
+void master()
+{
+    puts("I am the master!");
+}
+
+void slave()
+{
+    puts("But the 13th amendment!!!");
+}
+
+int main(int argc, char** argv)
+{
+    int nprocs;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
+    // One master and nprocs-1 slaves
+    slaves = nprocs - 1;
+
+    if (myid == MASTER_ID) {
+        fprintf(stderr, " +++ Process %d is master\n", myid);
+        master();
+    } else {
+        fprintf(stderr, " --- Process %d is slave\n", myid);
+        slave();
+    }
+    MPI_Finalize();
+    return 0;
+}
