3a511752f1e257bfee73e19b68e463e2d5662245
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-10-18 17:40:03 +0800

    Simplify train message-passing

--- train.c
+++ train.c
@@ -1,6 +1,5 @@
 #include "train.h"
 
-#define TAG 0
 #define TRAIN_NUM_FIELDS 3
 #define TRAIN_ID 0
 #define TRAIN_LINE_ID 1
@@ -16,17 +15,24 @@ void train_init(train_t* train, int id, int line_id, bool travelling_forward)
 void train_send(train_t* train, int dest)
 {
     int data[TRAIN_NUM_FIELDS];
-    data[TRAIN_ID] = train->id;
-    data[TRAIN_LINE_ID] = train->line_id;
-    data[TRAIN_TRAVELLING_FORWARD] = (int)train->travelling_forward;
-    MPI_Send(data, TRAIN_NUM_FIELDS, MPI_INT, dest, TAG, MPI_COMM_WORLD);
+    int tag = TRAIN_NO_TRAIN;
+
+    if (train != NULL) {
+        tag = TRAIN_GOT_TRAIN;
+        data[TRAIN_ID] = train->id;
+        data[TRAIN_LINE_ID] = train->line_id;
+        data[TRAIN_TRAVELLING_FORWARD] = (int)train->travelling_forward;
+    }
+    MPI_Send(data, TRAIN_NUM_FIELDS, MPI_INT, dest, tag, MPI_COMM_WORLD);
 }
 
-void train_recv(train_t* train, int source)
+int train_recv(train_t* train, int source)
 {
     int data[TRAIN_NUM_FIELDS];
-    MPI_Recv(data, TRAIN_NUM_FIELDS, MPI_INT, source, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Status* status;
+    MPI_Recv(data, TRAIN_NUM_FIELDS, MPI_INT, source, MPI_ANY_TAG, MPI_COMM_WORLD, status);
     train->id = data[TRAIN_ID];
     train->line_id = data[TRAIN_LINE_ID];
     train->travelling_forward = (bool)data[TRAIN_NUM_FIELDS];
+    return status->MPI_TAG;
 }
--- train.h
+++ train.h
@@ -3,6 +3,10 @@
 
 #include <mpi.h>
 #include <stdbool.h>
+#include <stddef.h>
+
+#define TRAIN_NO_TRAIN 0
+#define TRAIN_GOT_TRAIN 1
 
 typedef struct raw_train_t train_t;
 
@@ -14,7 +18,14 @@ struct raw_train_t {
 
 void train_init(train_t* train, int id, int line_id, bool travelling_forward);
 
+/**
+ * Set train to NULL to indicate that no train is sent.
+ */
 void train_send(train_t* train, int dest);
 
-void train_recv(train_t* train, int source);
+/**
+ * Returns 0 if there is no train (train_t* train contains garbage values)
+ * Returns 1 if a train is sent
+ */
+int train_recv(train_t* train, int source);
 #endif
