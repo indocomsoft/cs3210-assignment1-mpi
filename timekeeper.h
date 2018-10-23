64ba42f2dd03c7ba18e50d7a059ede6580fc7a3f
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-10-23 23:57:39 +0800

    Add comment to disambiguate return value

--- timekeeper.h
+++ timekeeper.h
@@ -14,6 +14,9 @@ void timekeeper_init(timekeeper_t* timekeeper);
 
 /**
  * Increases the timekeeper by duration and returns the new value.
+ *
+ * This new value is the starting time of the event.
+ * To get the ending time of the event, add duration to the return value.
  */
 int timekeeper_increase_by(timekeeper_t* timekeeper, int duration, int current_time);
 
